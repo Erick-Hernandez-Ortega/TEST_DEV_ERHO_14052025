3e7ab95c24ad2fba21892d0382363abe08a49de5
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-05-14 17:47:24 -0600

    feat: crear model

---
+++ APIPruebaToka/Models/PersonaFisica.cs
@@ -0,0 +1,13 @@
+﻿namespace APIPruebaToka.Models
+{
+    public class PersonaFisica
+    {
+        public int IdPersonaFisica { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string ApellidoPaterno { get; set; } = string.Empty;
+        public string ApellidoMaterno { get; set; } = string.Empty;
+        public string RFC { get; set; } = string.Empty;
+        public DateTime FechaNacimiento { get; set; }
+        public int UsuarioAgrega { get; set; }
+    }
+}
