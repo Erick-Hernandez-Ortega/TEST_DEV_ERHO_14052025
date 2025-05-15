383a44b386efa71a0ff31d299fedbd8ffcfb2c97
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-05-14 18:42:02 -0600

    feat: inicializar servicios

--- APIPruebaToka/Program.cs
+++ APIPruebaToka/Program.cs
@@ -1,3 +1,5 @@
+using APIPruebaToka.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IPersonaFisicaRepository, PersonaFisicaRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
