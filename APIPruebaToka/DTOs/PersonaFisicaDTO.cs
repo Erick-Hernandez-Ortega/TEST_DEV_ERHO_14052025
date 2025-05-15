b9c364aef04d7681448bf21bc0be48c2c8d395b2
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-05-14 18:47:56 -0600

    feat: crear persona fisica DTO

---
+++ APIPruebaToka/DTOs/PersonaFisicaDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace APIPruebaToka.DTOs
+{
+    public class PersonaFisicaDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string ApellidoPaterno { get; set; } = null!;
+        public string ApellidoMaterno { get; set; } = null!;
+        public string RFC { get; set; } = null!;
+        public DateTime FechaNacimiento { get; set; }
+        public string UsuarioAgrega { get; set; } = null!;
+    }
+}
