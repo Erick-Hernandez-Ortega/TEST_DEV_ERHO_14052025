3c2db7252c736d6b88a33c3bb57b0ffd0697097c
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-05-14 22:16:18 -0600

    feat: crear get de personas fisicas

---
+++ FrontPruebaToka/Controllers/PersonasFisicasController.cs
@@ -0,0 +1,35 @@
+﻿using FrontPruebaToka.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace FrontPruebaToka.Controllers
+{
+    public class PersonasFisicasController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PersonasFisicasController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("http://localhost:5268/api/PersonaFisica");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var persons = JsonSerializer.Deserialize<List<PersonaFisicaViewModel>>(json, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                return View(persons);
+            }
+
+            return View(new List<PersonaFisicaViewModel>());
+        }
+    }
+}
---
+++ FrontPruebaToka/Models/PersonaFisicaViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FrontPruebaToka.Models
+{
+    public class PersonaFisicaViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = "";
+        public string ApellidoPaterno { get; set; } = "";
+        public string ApellidoMaterno { get; set; } = "";
+        public string RFC { get; set; } = "";
+        public DateTime FechaNacimiento { get; set; }
+        public string UsuarioAgrega { get; set; } = "";
+    }
+}
