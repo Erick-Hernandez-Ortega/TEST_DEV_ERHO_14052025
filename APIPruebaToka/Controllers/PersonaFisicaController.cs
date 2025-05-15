c3737cb6a0749fac53f6ba96478968f655b1233c
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-05-14 18:41:12 -0600

    feat: crear persona fisica controller

---
+++ APIPruebaToka/Controllers/PersonaFisicaController.cs
@@ -0,0 +1,29 @@
+﻿using APIPruebaToka.DTOs;
+using APIPruebaToka.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIPruebaToka.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonaFisicaController : ControllerBase
+    {
+        private readonly IPersonaFisicaRepository _repo;
+
+        public PersonaFisicaController(IPersonaFisicaRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear([FromBody] CreatePersonaFisica dto)
+        {
+            var (error, mensaje) = await _repo.CreatePersonaFisicaAsync(dto);
+
+            if (error < 0)
+                return BadRequest(new { error, mensaje });
+
+            return Ok(new { id = error, mensaje });
+        }
+    }
+}
