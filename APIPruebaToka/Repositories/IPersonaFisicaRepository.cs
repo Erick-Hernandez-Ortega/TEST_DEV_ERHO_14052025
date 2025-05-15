34f0f1ae6adf060b38994a182bcb8d1f906ac0a1
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-05-14 18:01:01 -0600

    feat: crear interface

---
+++ APIPruebaToka/Repositories/IPersonaFisicaRepository.cs
@@ -0,0 +1,9 @@
+﻿using APIPruebaToka.DTOs;
+
+namespace APIPruebaToka.Repositories
+{
+    public interface IPersonaFisicaRepository
+    {
+        Task<(int Error, string Mensaje)> CreatePersonaFisicaAsync(CreatePersonaFisica dto);
+    }
+}
