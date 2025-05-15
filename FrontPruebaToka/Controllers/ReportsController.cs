38748779c00d2c94f1b34ab0138ee9922b425ea4
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-05-14 23:16:13 -0600

    feat: crear vista de reports

---
+++ FrontPruebaToka/Controllers/ReportsController.cs
@@ -0,0 +1,14 @@
+﻿using FrontPruebaToka.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontPruebaToka.Controllers
+{
+    public class ReportsController : Controller
+    {
+
+        public Task<IActionResult> Index()
+        {
+            return Task.FromResult<IActionResult>(View());
+        }
+    }
+}
